c55cdfc11f59a383c224d8067d1dd917c9cb9779
ShivaTejaHitam <61039007+ShivaTejaHitam@users.noreply.github.com>
2021-12-31 16:57:37 +0530

    Update gcd_of_an_array.java

--- gcd_of_an_array.java
+++ gcd_of_an_array.java
@@ -1,11 +1,17 @@
 
-int gcd()
+int GCD(int a,int b)
 {
-
+    if(b==0)
+        return a;
+    return GCD(b,a%b);
 }
 
 int gcd_of_an_array(int[] arr)
 {
-    
-
+    int gcd=GCD(arr[0],arr[1]);
+    for(int i=2;i<arr.length;i++)
+    {
+        gcd=GCD(gcd,arr[i]);
+    }
+    return gcd;
 }
