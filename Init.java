ae76968bea8dc5c4b808b4f091b1320bfbf0137e
ShivaTejaHitam <61039007+ShivaTejaHitam@users.noreply.github.com>
2022-01-03 23:11:18 +0530

    Create Init.java

---
+++ Init.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+
+public class Ex
+{
+    
+    static int[] convert(String s)
+    {
+        String[] str=s.split(" ");
+        int[] num=new int[str.length];
+        for(int i=0;i<str.length;i++)
+        {
+            num[i]=Integer.parseInt(str[i]);
+        }     
+        return num;
+    }
+
+
+    public static void main(String args[])
+    {
+
+        Scanner sc=new Scanner(System.in);
+	      int tc=convert(sc.nextLine());
+	
+        for(int t=0;t<tc;t++)
+        {
+
+
+        }
+    }
+}
+
