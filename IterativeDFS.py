754b211bd2fb8191825f004a67943abfec422894
ShivaTejaHitam <61039007+ShivaTejaHitam@users.noreply.github.com>
2022-01-17 13:51:31 +0530

    Update IterativeDFS.py

--- IterativeDFS.py
+++ IterativeDFS.py
@@ -1,15 +1,15 @@
 def DFS(graph,startnode):
-    visited=[startnode]
+    visited={startnode:1}
     stack=[startnode]
 
     while(len(stack)!=0):
         vertex=stack.pop()
         print(vertex)
         for node in graph[vertex]:
-            if node not in visited:
-                visited.append(node)
+            if visited.has_key(node)==False:
+                visited[node]=True
                 stack.append(node)
-
+                
 graph={'a':['b','c'],'b':['a','d'],'c':['a','d'],'d':['e'],'e':['a']}
 
 DFS(graph,'a')
