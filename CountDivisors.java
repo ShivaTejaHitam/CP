6998f917c5470d13df0d8bc82ddbbd928a0982bd
ShivaTejaHitam <61039007+ShivaTejaHitam@users.noreply.github.com>
2022-01-25 23:22:01 +0530

    Update CountDivisors.java

--- CountDivisors.java
+++ CountDivisors.java
@@ -1,1 +1,13 @@
 
+static int countDivisors(int number)
+{
+  int count = 0;
+  for(int i = 0 ;i <= Math.ceil(Math.sqrt(number)) ; i++ )
+  {
+      if(number % i == 0)
+        count++;
+      if(i != n/i)
+        count++;
+  }
+  return count;
+}
