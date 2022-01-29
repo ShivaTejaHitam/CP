faad29ba36dfae44c4c1f30fdbc29f868740aa3a
ShivaTejaHitam <61039007+ShivaTejaHitam@users.noreply.github.com>
2022-01-29 14:40:54 +0530

    Update KMP.java

--- KMP.java
+++ KMP.java
@@ -1,1 +1,48 @@
-hello
+void KMP(String text,String pattern)
+{
+  int text_length = text.length();
+  int pattern_length = pattern_length();
+  
+  
+  
+}
+
+
+
+
+
+
+int[] lps(String s,int length) {
+        
+        int[] lps = new int[length];
+        int prev = 0;
+        int i = 1; 
+        lps[0] = 0;
+        
+        while(i < length)
+        {
+            if(s.charAt(i) == s.charAt(prev))
+            {
+                prev++;
+                lps[i] = prev;
+                i++;
+            }
+            else
+            {
+                if(prev != 0)
+                {
+                    prev = lps [prev - 1];
+                }
+                
+                else
+                {
+                    lps[i] = prev;
+                    i++;
+                }
+                
+            }
+            
+        }
+   // return prev; for returning length of LPS
+   return lps; 
+}
