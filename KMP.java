7fc044e40539b19b6dd341c8fd854784b7168c95
ShivaTejaHitam <61039007+ShivaTejaHitam@users.noreply.github.com>
2022-01-29 15:05:25 +0530

    Update KMP.java

--- KMP.java
+++ KMP.java
@@ -3,8 +3,31 @@ void KMP(String text,String pattern)
   int text_length = text.length();
   int pattern_length = pattern_length();
   
+  int lps[] = get_lps(pattern,pattern_length);
+  int j = 0;
+  int i = 0;
   
-  
+  while(i < text_length)
+  {
+    if(pattern.charAt(j) == text.charAt(i))
+    {
+       i++;
+       j++;
+    }
+    if(j == pattern_length)
+    {
+       System.out.println("Pattern found at location " + (i - j));
+       j = lps[j - 1];
+    }
+ 
+    else if(i < text_length && pattern.charAt(j) != text.charAt(i))
+    {
+       if(j != 0)
+          j = lps[j - 1];
+       else 
+          i = i + 1;
+    }
+  }
 }
 
 
@@ -12,7 +35,7 @@ void KMP(String text,String pattern)
 
 
 
-int[] lps(String s,int length) {
+int[] get_lps(String s,int length) {
         
         int[] lps = new int[length];
         int prev = 0;
