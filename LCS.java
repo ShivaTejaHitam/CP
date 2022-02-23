d7ac3bb3b0e2ee918382c0f6313d4f868e3e0745
ShivaTejaHitam <61039007+ShivaTejaHitam@users.noreply.github.com>
2022-02-23 10:59:57 +0530

    Update LCS.java

--- LCS.java
+++ LCS.java
@@ -1,7 +1,5 @@
-public int longestCommonSubsequence(String text1, String text2) 
+static int longestCommonSubsequence(String text1, String text2) 
 {
-        
-        
         int m = text1.length();
         int n = text2.length();
         
