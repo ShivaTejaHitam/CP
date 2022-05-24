f8b309123d4fef1899d9defff5698a69d85950b9
ShivaTejaHitam <61039007+ShivaTejaHitam@users.noreply.github.com>
2022-05-24 14:23:23 +0530

    Update PalindromePartitioning.java

--- PalindromePartitioning.java
+++ PalindromePartitioning.java
@@ -1,9 +1,15 @@
 class Solution {
+    static int[][] dp = new int[2001][2001];
     
-    public boolean isPalindrome(String string,int i,int j){
+    public boolean isPalindrome(char[] string,int i,int j){
+        
+        if(i==j || i>j)
+            return true;
+        
+        
         while(i < j)
         {
-            if(string.charAt(i) != string.charAt(j))
+            if(string[i] != string[j])
                  return false; 
             i++;
             j--;
@@ -11,25 +17,53 @@ class Solution {
         return true;
     }
     
-    public int palindromePartition(String s,int start,int end){
-        if(start>=end || isPalindrome(s,start,end)){
+    public int palindromePartition(String s,int start,int end,int[][] dp){
+        if(start>=end){
             return 0;
         }
+        
+        
+        if(dp[start][end]!=-1){
+            return dp[start][end];
+        }
+        
+        if(isPalindrome(s.toCharArray(),start,end)){
+            dp[start][end] = 0;
+            return 0;
+        }
+        
         
         int minCuts = Integer.MAX_VALUE;
         int temp;
         for(int k = start ; k < end; k++){
-             temp = 1 + palindromePartition(s,start,k)+palindromePartition(s,k+1,end);
+             int left = dp[start][k];
+             int right = dp[k+1][end];
+             if(dp[start][k]==-1){
+                 dp[start][k] = palindromePartition(s,start,k,dp);
+                 left = dp[start][k];
+             }
+            
+             if(dp[k+1][end]==-1){
+                 dp[k+1][end] = palindromePartition(s,k+1,end,dp);
+                 right = dp[k+1][end];
+             }
+            temp = 1 + left + right;
             minCuts = Math.min(minCuts,temp);
         }
         
-        return minCuts;
+        return dp[start][end]=minCuts;
     }
     
     
     
     public int minCut(String s) {
-        return palindromePartition(s,0,s.length()-1);
         
+        for(int i = 0 ; i <= s.length() ; i++){
+            for(int j = 0 ; j <= s.length() ;j++){
+                dp[i][j] = -1;
+            }
+        }
+        
+        return palindromePartition(s,0,s.length()-1,dp);
     }
 }
