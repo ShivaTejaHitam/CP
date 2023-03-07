14995bc349d09b2e00c71c6bc7e3f8dc48a613e7
ShivaTejaHitam <61039007+ShivaTejaHitam@users.noreply.github.com>
2023-03-07 13:34:37 +0530

    Update DFSRecursive.java

--- Graphs/DFSRecursive.java
+++ Graphs/DFSRecursive.java
@@ -2,9 +2,9 @@
 static void DFS(Map<Integer,ArrayList<Integer>> graph,int source){
   visited[source] = true;
   
-  for(Integer node : graph.get(source)){
-    if(visited[node]==false){
-      DFS(graph,node);
+  for(Integer vertex : graph.get(source)){
+    if(visited[vertex]==false){
+      DFS(graph,vertex);
     }
   }
   
