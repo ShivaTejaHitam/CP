a04c75aff919f5cd9a366c1c1c7befed42231e84
Shiva Teja Bandaru <61039007+ShivaTejaHitam@users.noreply.github.com>
2024-10-17 07:17:55 +0530

    Create MonotonicStack.java

---
+++ MonotonicStack.java
@@ -0,0 +1,17 @@
+class DailyTemperatures {
+    public int[] dailyTemperatures(int[] temperatures) {
+        Stack<Integer> stack = new Stack<>();
+
+        int[] result = new int[temperatures.length];
+
+        for(int  i = 0 ; i < temperatures.length ; i++){
+            while(!stack.empty() && temperatures[i] > temperatures[stack.peek()]){
+                int index = stack.pop();
+                result[index] = i - index;
+            }
+            stack.push(i);
+        }
+
+        return result;
+    }
+}
