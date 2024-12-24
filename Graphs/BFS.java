648204b6855c82ef6230037d31ab71b1a37fc2c9
Shiva Teja Bandaru <61039007+ShivaTejaHitam@users.noreply.github.com>
2024-12-24 19:59:25 +0530

    Update BFS.java

--- Graphs/BFS.java
+++ Graphs/BFS.java
@@ -1,9 +1,16 @@
 public void BFS(Map<Integer,List<Integer>> graph, int source) {
     Queue<Integer> queue = new LinkedList<>();
+    Set<Integer> visited = new HashSet<>();
 
     queue.offer(source);
 
     while(!queue.isEmpty()){
-      int visited = 
+      int currentElement = queue.poll();
+      System.out.println(currentElement);
+      for(int i : graph.get(currentElement)){
+          if(visited.contains(i) == false){
+             queue.offer(i);
+          }
+      }
     }
 }
