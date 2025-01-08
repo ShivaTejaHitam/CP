ec5199bb62a666b5a9eb88ccb84c62ab0b8f366b
Shiva Teja Bandaru <61039007+ShivaTejaHitam@users.noreply.github.com>
2025-01-08 11:27:42 +0530

    Update BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,13 +1,18 @@
 class BubbleSort{
   public void bubbleSort(int[] arr,int n){
     for(int i = 0 ; i < n-1 ; i++){
+      boolean swapped = false;
       for(int j = 0 ; j < n-i-1 ; j++){
         if(arr[j+1] < arr[j]){
            int temp = arr[j+1];
            arr[j+1] = arr[j];
            arr[j] = temp;
+          swapped = true;
         }
     }
+      if(swapped == false){
+         break; // the array is already sorted
+      }
   }
 }
 
