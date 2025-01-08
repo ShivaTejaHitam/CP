c1cd0dcae18a745f2ca00502901c1863ac736348
Shiva Teja Bandaru <61039007+ShivaTejaHitam@users.noreply.github.com>
2025-01-08 11:23:59 +0530

    Create BubbleSort.java

---
+++ BubbleSort.java
@@ -0,0 +1,13 @@
+class BubbleSort{
+  public void bubbleSort(int[] arr,int n){
+    for(int i = 0 ; i < n-1 ; i++){
+      for(int j = 0 ; j < n-i-1 ; j++){
+        if(arr[j+1] < arr[j]){
+           int temp = arr[j+1];
+           arr[j+1] = arr[j];
+           arr[j] = temp;
+        }
+    }
+  }
+}
+
