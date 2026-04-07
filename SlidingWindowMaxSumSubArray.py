0d09d4adc251fdf0b8bd31aef685483ea2b78a45
Shiva Teja Bandaru <61039007+ShivaTejaHitam@users.noreply.github.com>
2026-04-07 15:38:27 +0530

    Update SlidingWindowMaxSumSubArray.py

--- SlidingWindowMaxSumSubArray.py
+++ SlidingWindowMaxSumSubArray.py
@@ -81,3 +81,32 @@ public int longestKSubstr(String s, int k) {
         
         return result;
     }
+
+
+*******************************************************************************************
+class Solution {
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        return atMost(nums, goal) - atMost(nums, goal - 1);
+    }
+
+    private int atMost(int[] nums, int goal) {
+        if (goal < 0) return 0;
+
+        int start = 0;
+        int sum = 0;
+        int count = 0;
+
+        for (int end = 0; end < nums.length; end++) {
+            sum += nums[end];
+
+            while (sum > goal) {
+                sum -= nums[start];
+                start++;
+            }
+
+            count += end - start + 1;
+        }
+
+        return count;
+    }
+}
