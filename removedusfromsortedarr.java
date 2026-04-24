6c136ff4bf0599bee9116fd5d1a72cb14c4a3522
Shiva Teja Bandaru <61039007+ShivaTejaHitam@users.noreply.github.com>
2026-04-24 17:22:33 +0530

    Create removedusfromsortedarr.java

---
+++ removedusfromsortedarr.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int length = nums.length;
+        if(length == 1) return 1;
+
+        int i = 0;
+
+        for(int j = 1 ; j < length ; j++){
+            if(nums[j] != nums[i]){
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+
+        return i+1;
+    }
+}
